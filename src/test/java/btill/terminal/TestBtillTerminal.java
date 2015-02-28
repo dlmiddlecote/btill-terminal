a92d25d1111ca3548daacd9e65e5a4587fd1e544
Leila <ls2914@ic.ac.uk>
2015-02-28 14:41:53 +0000

    Testing CI pipeline.

--- src/test/java/btill/terminal/TestBtillTerminal.java
+++ src/test/java/btill/terminal/TestBtillTerminal.java
@@ -1,12 +1,14 @@
 package btill.terminal;
 
-import btill.terminal.values.*;
-import com.google.gson.Gson;
-import org.bitcoin.protocols.payments.Protos.Payment;
-import org.junit.Test;
+import btill.terminal.values.GBP;
+import btill.terminal.values.Menu;
+import btill.terminal.values.MenuItem;
 
 import static java.util.Arrays.asList;
-import static org.hamcrest.MatcherAssert.assertThat;
+
+//import com.google.gson.Gson;
+//import org.bitcoin.protocols.payments.Protos.Payment;
+//import org.junit.Test;
 
 public class TestBtillTerminal {
 
@@ -14,29 +16,29 @@ public class TestBtillTerminal {
     MenuItem fakeItem = new MenuItem("fakeItem", price);
     private Menu fakeMenu = createMenu();
 
-
-    @Test public void something() {
-        FakeServer server = new FakeServer();
-        FakeTill till = new FakeTill();
-        BtillTerminal terminal = new BtillTerminal();
-        terminal.startUsing(server, till);
-        server.sendRequest();
-    }
-
-    private class FakeServer implements Server {
-        private Controller controller;
-        @Override public void start() {}
-        @Override public void use(Controller controller) {
-            this.controller = controller;
-        }
-        @Override public void close() throws Exception {}
-
-        public void sendRequest() {
-            // Order order = new Order(fakeMenu);
-//            String serializedItem = new Gson().toJson(new Order());
-
-        }
-    }
+//
+//    @Test public void something() {
+//        FakeServer server = new FakeServer();
+//        FakeTill till = new FakeTill();
+//        BtillTerminal terminal = new BtillTerminal();
+//        terminal.startUsing(server, till);
+//        server.sendRequest();
+//    }
+//
+//    private class FakeServer implements Server {
+//        private Controller controller;
+//        @Override public void start() {}
+//        @Override public void use(Controller controller) {
+//            this.controller = controller;
+//        }
+//        @Override public void close() throws Exception {}
+//
+//        public void sendRequest() {
+//            // Order order = new Order(fakeMenu);
+////            String serializedItem = new Gson().toJson(new Order());
+//
+//        }
+//    }
 
     private Menu createMenu() {
         return new Menu(asList(
