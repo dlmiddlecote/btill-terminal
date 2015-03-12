0f4e09a3708b17b411ac7176c55f2416bbafb5ca
Leila <ls2914@ic.ac.uk>
2015-03-12 21:53:33 +0000

    added tests for Server and Terminal.

---
+++ src/test/java/btill/terminal/BtillServerTest.java
@@ -0,0 +1,108 @@
+package btill.terminal;
+
+import btill.terminal.values.BTMessage;
+import btill.terminal.values.Menu;
+import com.google.gson.Gson;
+import org.junit.Test;
+import org.spongycastle.asn1.DERBitString;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class BtillServerTest {
+    private static final String SERVICE_URI = "someUri";
+    private final Controller controller = new DummyController();
+    private final BtillServer server = new BtillServer(SERVICE_URI);
+
+
+    @Test public void canListenForConnections() {
+        server.use(new DummyController());
+        server.listenForConnectionsOn(dummyService());
+    }
+
+    private DummyStreamConnectionNotifier dummyService() {
+        return new DummyStreamConnectionNotifier();
+    }
+
+    private class DummyController extends Controller {
+        public DummyController() {
+            super(null, null);
+        }
+
+        public BTMessage processRequest(Command command, byte[] content) {
+            return exampleMessage();
+        }
+    }
+
+    private class DummyStreamConnectionNotifier implements StreamConnectionNotifier {
+        int connectionsRequested = 0;
+        private boolean closed = false;
+
+        @Override
+        public StreamConnection acceptAndOpen() throws IOException {
+            if (connectionsRequested++ > 1) {
+                return null;
+            }
+            return new DummyConnection();
+        }
+
+        @Override
+        public void close() throws IOException {
+            closed = true;
+        }
+
+        public boolean wasClosed() {
+            return closed ;
+        }
+    }
+
+
+    private class DummyConnection implements StreamConnection {
+        private boolean wasClosed = false;
+
+        @Override
+        public InputStream openInputStream() throws IOException {
+            return  new ByteArrayInputStream(asJsonString(exampleMessage()).getBytes(StandardCharsets.UTF_8));
+        }
+
+        @Override
+        public OutputStream openOutputStream() throws IOException {
+            return new OutputStream() {
+                @Override
+                public void write(int b) throws IOException {
+
+                }
+            };
+        }
+
+        @Override
+        public DataInputStream openDataInputStream() throws IOException {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public DataOutputStream openDataOutputStream() throws IOException {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public void close() throws IOException {
+            wasClosed = true;
+        }
+    }
+
+    private String asJsonString(BTMessage msg) {
+        return new Gson().toJson(msg);
+    }
+
+    private BTMessage exampleMessage() {
+        return (new BTMessage(Command.REQUEST_MENU.toString(), "".getBytes()));
+    }
+}
---
+++ src/test/java/btill/terminal/bitcoin/TestSignedBill.java
@@ -0,0 +1,129 @@
+package btill.terminal.bitcoin;
+
+import btill.terminal.values.Bill;
+import btill.terminal.values.GBP;
+import btill.terminal.values.SignedBill;
+import org.bitcoin.protocols.payments.Protos;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.Wallet;
+import org.bitcoinj.protocols.payments.PaymentProtocolException;
+import org.bitcoinj.protocols.payments.PaymentSession;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Adam Kent on 08/03/2015.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestSignedBill {
+
+    private Protos.Payment testPayment;
+    private WalletKitThread testWalletKitThread = new WalletKitThread("resources/bitcoin_test_files", "testSignedBill");
+
+    private Coin testAmount = Coin.valueOf(17);
+    private GBP testGbpAmount = new GBP(29);
+    private String testMemo = "testMemo";
+    private String testPaymentURL = "testPaymentURL";
+    private byte[] testMerchantData = String.format("testMerchantData").getBytes();
+
+    @Test
+    public void signedBillAAAConstructor() throws InsufficientMoneyException, IOException, PaymentProtocolException {
+        testWalletKitThread.run();
+
+        signedBillHelper();
+
+        SignedBill test = new SignedBill(testPayment, testGbpAmount, testAmount);
+
+        while (testWalletKitThread.isRunning())
+            testWalletKitThread.terminate();
+
+        assertThat(test, notNullValue());
+    }
+
+    @Test
+    public void signedBillGetBTCAmount() throws InsufficientMoneyException, IOException, PaymentProtocolException {
+        testWalletKitThread.run();
+
+        signedBillHelper();
+
+        SignedBill test = new SignedBill(testPayment, testGbpAmount, testAmount);
+
+        while (testWalletKitThread.isRunning())
+            testWalletKitThread.terminate();
+
+        assertThat(test.getBtcAmount(), equalTo(testAmount));
+    }
+
+    @Test
+    public void signedBillGetGBPAmount() throws InsufficientMoneyException, IOException, PaymentProtocolException {
+        testWalletKitThread.run();
+
+        signedBillHelper();
+
+        SignedBill test = new SignedBill(testPayment, testGbpAmount, testAmount);
+
+        while (testWalletKitThread.isRunning())
+            testWalletKitThread.terminate();
+
+        assertThat(test.getGbpAmount(), equalTo(testGbpAmount));
+    }
+
+    @Test
+    public void signedBillGetSerialisedPayment() throws InsufficientMoneyException, IOException, PaymentProtocolException {
+        testWalletKitThread.run();
+
+        signedBillHelper();
+
+        SignedBill test = new SignedBill(testPayment, testGbpAmount, testAmount);
+
+        while (testWalletKitThread.isRunning())
+            testWalletKitThread.terminate();
+
+        assertThat(test.getSerialisedPayment(), equalTo(testPayment.toByteArray()));
+    }
+
+    @Test
+    public void signedBillGetPayment() throws InsufficientMoneyException, IOException, PaymentProtocolException {
+        testWalletKitThread.run();
+
+        signedBillHelper();
+
+        SignedBill test = new SignedBill(testPayment, testGbpAmount, testAmount);
+
+        while (testWalletKitThread.isRunning())
+            testWalletKitThread.terminate();
+
+        assertThat(test.getPayment(), equalTo(testPayment));
+    }
+
+    public void signedBillHelper() throws InsufficientMoneyException, IOException, PaymentProtocolException {
+        Bill testBill = new Bill(testMemo, testPaymentURL, testMerchantData, testAmount, testGbpAmount, testWalletKitThread.getWalletAppKit().wallet(), false);
+
+        PaymentSession testPaymentSession = new PaymentSession(testBill.getRequest(), false);
+
+        Wallet.SendRequest testSendRequest = testPaymentSession.getSendRequest();
+
+        testSendRequest.ensureMinRequiredFee = false;
+        testSendRequest.fee = Coin.ZERO;
+        testSendRequest.feePerKb = Coin.ZERO;
+        testSendRequest.signInputs = false;
+
+        testWalletKitThread.getWalletAppKit().wallet().completeTx(testSendRequest);
+        testWalletKitThread.getWalletAppKit().wallet().signTransaction(testSendRequest);
+        List<Transaction> txil = new ArrayList<Transaction>();
+        txil.add(testSendRequest.tx);
+
+        testPayment = testPaymentSession.getPayment(txil, testWalletKitThread.getWalletAppKit().wallet().currentReceiveAddress(), testPaymentSession.getMemo());
+    }
+}
